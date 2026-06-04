f0d93575bb5fff2765f1d883caaf0519c3c9c6fe
Legopitstop <1589lego@gmail.com>
2026-06-03 23:48:23 -0500

    feat(mc-dev): add game mode macro intent

---
+++ libraries/mc-dev/src/macro/intents/game_mode.ts
@@ -0,0 +1,19 @@
+import { Player } from "@minecraft/server";
+import { MacroIntent, MacroIntentData } from "./base";
+
+export class ToggleGameModeIntent extends MacroIntent {
+  static readonly intentId = "toggle_game_mode";
+
+  constructor() {
+    super(ToggleGameModeIntent.intentId, {
+      name: "Toggle Game Mode",
+    });
+  }
+
+  execute(player: Player, options: MacroIntentData): void {
+    player.sendMessage("Action not implemented.");
+  }
+}
+
+// Initialize
+new ToggleGameModeIntent();
