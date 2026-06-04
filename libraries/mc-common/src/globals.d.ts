c29129df753645b2ffe580384a7fde7e15cfa5b5
Legopitstop <1589lego@gmail.com>
2026-06-03 23:50:07 -0500

    types(mc-common): add global declarations

---
+++ libraries/mc-common/src/globals.d.ts
@@ -0,0 +1,7 @@
+declare const console: {
+  log(...data: unknown[]): void;
+  warn(...data: unknown[]): void;
+  error(...data: unknown[]): void;
+  info(...data: unknown[]): void;
+  debug(...data: unknown[]): void;
+};
