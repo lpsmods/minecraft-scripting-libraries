93a7a9db91d0f9903a53208779e57f24fdec9a73
Legopitstop <1589lego@gmail.com>
2026-06-03 23:50:08 -0500

    feat(mc-common): export event and matrix modules

--- libraries/mc-common/src/index.ts
+++ libraries/mc-common/src/index.ts
@@ -1,11 +1,15 @@
+/*! https://lnk.lpsmods.dev/mc-common */
+/// <reference path="./globals.d.ts" />
+
 export * from "./data";
 export * from "./ui";
+export * from "./event";
 
 export * from "./constants";
 export * from "./error";
-export * from "./event";
 export * from "./identifier";
 export * from "./math";
+export * from "./matrix";
 export * from "./settings";
 export * from "./text";
 export * from "./ticking";
