aa832fc7230ed838e619b0453975185e82182a7e
Legopitstop <1589lego@gmail.com>
2026-06-03 23:48:23 -0500

    feat(mc-dev): add macro command

---
+++ libraries/mc-dev/src/command/macro.ts
@@ -0,0 +1,34 @@
+import {
+  CommandPermissionLevel,
+  CustomCommand,
+  CustomCommandOrigin,
+  CustomCommandRegistry,
+  CustomCommandResult,
+} from "@minecraft/server";
+import { DeveloperTools } from "../developer_tools";
+import { Macros } from "../macro";
+import { CustomCommandUtils } from "@lpsmods/mc-utils";
+
+export class MacroCommand {
+  private static registered: boolean = false;
+
+  static execute(macro: number, ctx: CustomCommandOrigin): CustomCommandResult | undefined {
+    const player = CustomCommandUtils.getPlayer(ctx);
+    Macros.execute(macro, player);
+    return undefined;
+  }
+
+  static register(registry: CustomCommandRegistry): void {
+    if (this.registered) return;
+    for (let i = 1; i <= 10; i++) {
+      const namespace = DeveloperTools.instance?.devOptions.namespace ?? "mcdev";
+      const options: CustomCommand = {
+        name: `${namespace}:macro${i}`,
+        description: `Run macro ${i}.`,
+        permissionLevel: CommandPermissionLevel.Admin,
+      };
+      registry.registerCommand(options, (ctx) => MacroCommand.execute(i, ctx));
+    }
+    this.registered = true;
+  }
+}
