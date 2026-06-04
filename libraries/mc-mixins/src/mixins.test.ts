ef615d0b1975a65bd40e31f0bfb6348f5cc4fa3b
Legopitstop <1589lego@gmail.com>
2026-06-03 23:46:50 -0500

    test(mc-mixins): expand mixin coverage

--- libraries/mc-mixins/src/mixins.test.ts
+++ libraries/mc-mixins/src/mixins.test.ts
@@ -3,6 +3,7 @@ import { describe, it, expect } from "vitest";
 
 // You need to import it
 import "./mixins";
+import { Chunk } from "@lpsmods/mc-utils";
 
 describe("ItemStack mixin", () => {
   const obj = new ItemStack("paper");
@@ -21,73 +22,84 @@ describe("ScoreboardObjective mixin", () => {
 });
 describe("Block mixin", () => {
   const obj = world.getDimension("overworld").getBlock({ x: 0, y: 0, z: 0 });
+  if (!obj) throw new Error("Block not found");
   it("should have executeMolang", () => {
-    expect(typeof (obj as any).executeMolang).toBe("function");
+    expect(typeof obj.executeMolang).toBe("function");
   });
   it("should have clearDynamicProperties", () => {
-    expect(typeof (obj as any).clearDynamicProperties).toBe("function");
+    expect(typeof obj.clearDynamicProperties).toBe("function");
   });
   it("should have getDynamicProperty", () => {
-    expect(typeof (obj as any).getDynamicProperty).toBe("function");
+    expect(typeof obj.getDynamicProperty).toBe("function");
   });
   it("should have getDynamicPropertyIds", () => {
-    expect(typeof (obj as any).getDynamicPropertyIds).toBe("function");
+    expect(typeof obj.getDynamicPropertyIds).toBe("function");
   });
   it("should have getDynamicPropertyTotalByteCount", () => {
-    expect(typeof (obj as any).getDynamicPropertyTotalByteCount).toBe("function");
+    expect(typeof obj.getDynamicPropertyTotalByteCount).toBe("function");
   });
   it("should have setDynamicProperty", () => {
-    expect(typeof (obj as any).setDynamicProperty).toBe("function");
+    expect(typeof obj.setDynamicProperty).toBe("function");
   });
   it("should have getState", () => {
-    expect(typeof (obj as any).getState).toBe("function");
+    expect(typeof obj.getState).toBe("function");
   });
   it("should have setState", () => {
-    expect(typeof (obj as any).setState).toBe("function");
+    expect(typeof obj.setState).toBe("function");
   });
   it("should have incrementState", () => {
-    expect(typeof (obj as any).incrementState).toBe("function");
+    expect(typeof obj.incrementState).toBe("function");
   });
   it("should have decrementState", () => {
-    expect(typeof (obj as any).decrementState).toBe("function");
+    expect(typeof obj.decrementState).toBe("function");
   });
+  // it("should have chunk", () => {
+  //   expect(obj.chunk instanceof Chunk).toBe(true);
+  // });
 });
 describe("Player mixin", () => {
   const obj = world.getPlayers()[0];
   it("should have applyArmor", () => {
-    expect(typeof (obj as any).applyArmor).toBe("function");
+    expect(typeof obj.applyArmor).toBe("function");
   });
 });
 describe("Entity mixin", () => {
   const obj = world.getEntity("0");
+  if (!obj) throw new Error("Entity not found");
   it("should have executeMolang", () => {
-    expect(typeof (obj as any).executeMolang).toBe("function");
+    expect(typeof obj.executeMolang).toBe("function");
   });
+  it("should have isMoving", () => {
+    expect(typeof obj.isMoving).toBe("boolean");
+  });
+  // it("should have chunk", () => {
+  //   expect(obj.chunk instanceof Chunk).toBe(true);
+  // });
 });
 describe("String mixin", () => {
   const obj = "hello world";
   it("should have toSmartTitleCase", () => {
-    expect(typeof (obj as any).toSmartTitleCase).toBe("function");
+    expect(typeof obj.toSmartTitleCase).toBe("function");
   });
   it("should have toTitleCase", () => {
-    expect(typeof (obj as any).toTitleCase).toBe("function");
+    expect(typeof obj.toTitleCase).toBe("function");
   });
   it("should have toCamelCase", () => {
-    expect(typeof (obj as any).toCamelCase).toBe("function");
+    expect(typeof obj.toCamelCase).toBe("function");
   });
   it("should have toParamCase", () => {
-    expect(typeof (obj as any).toParamCase).toBe("function");
+    expect(typeof obj.toParamCase).toBe("function");
   });
   it("should have toPascalCase", () => {
-    expect(typeof (obj as any).toPascalCase).toBe("function");
+    expect(typeof obj.toPascalCase).toBe("function");
   });
   it("should have toSnakeCase", () => {
-    expect(typeof (obj as any).toSnakeCase).toBe("function");
+    expect(typeof obj.toSnakeCase).toBe("function");
   });
   it("should have truncate", () => {
-    expect(typeof (obj as any).truncate).toBe("function");
+    expect(typeof obj.truncate).toBe("function");
   });
   it("should have reverse", () => {
-    expect(typeof (obj as any).reverse).toBe("function");
+    expect(typeof obj.reverse).toBe("function");
   });
 });
