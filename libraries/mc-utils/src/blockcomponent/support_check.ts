470d882e4c3815da8385c49083fba31cf9c21cc7
Legopitstop <1589lego@gmail.com>
2026-06-03 23:41:24 -0500

    feat(mc-utils): add support check block component

---
+++ libraries/mc-utils/src/blockcomponent/support_check.ts
@@ -0,0 +1,6 @@
+import { AddonUtils } from "../utils";
+import { BlockBaseComponent } from "./base";
+
+export class SupportCheckComponent extends BlockBaseComponent {
+  static readonly componentId = AddonUtils.makeId("stairs");
+}
