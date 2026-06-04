a8dc6a7deffdb2417418d74df49451ab3e60eb87
Legopitstop <1589lego@gmail.com>
2026-06-03 23:41:24 -0500

    feat(mc-utils): add tnt block component

---
+++ libraries/mc-utils/src/blockcomponent/tnt.ts
@@ -0,0 +1,59 @@
+import {
+  Block,
+  BlockComponentPlayerInteractEvent,
+  BlockComponentRedstoneUpdateEvent,
+  BlockCustomComponent,
+  CustomComponentParameters,
+} from "@minecraft/server";
+import { defaulted, number, object, string, Struct } from "superstruct";
+
+import { AddonUtils } from "../utils/addon";
+import { ItemUtils } from "../item";
+import { BlockUtils } from "../block/utils";
+
+export interface TntBlockOptions {
+  entity: string;
+  y_rotation_offset: number;
+}
+
+export class TntBlockComponent implements BlockCustomComponent {
+  static readonly componentId = AddonUtils.makeId("tnt");
+  struct: Struct<any, any> = object({
+    entity: defaulted(string(), "tnt"),
+    y_rotation_offset: defaulted(number(), 0),
+  });
+
+  /**
+   * Vanilla time detector block behavior. (like; daylight detector)
+   *
+   * Requires `minecraft:redstone_consumer`
+   */
+  constructor() {
+    this.onPlayerInteract = this.onPlayerInteract.bind(this);
+    this.onRedstoneUpdate = this.onRedstoneUpdate.bind(this);
+  }
+
+  ignite(block: Block, options: TntBlockOptions): void {
+    const blockId = block.typeId; // Get ID before block change.
+    const entity = BlockUtils.spawnBlockEntity(block, options.entity, {
+      initialRotation: options.y_rotation_offset,
+      removeBlock: true,
+    });
+    try {
+      entity.triggerEvent(blockId);
+    } catch (err) {}
+  }
+
+  // EVENTS
+
+  onRedstoneUpdate(event: BlockComponentRedstoneUpdateEvent, args: CustomComponentParameters): void {
+    const options = this.struct.create(args.params) as TntBlockOptions;
+    this.ignite(event.block, options);
+  }
+
+  onPlayerInteract(event: BlockComponentPlayerInteractEvent, args: CustomComponentParameters): void {
+    const options = this.struct.create(args.params) as TntBlockOptions;
+    if (!event.player || !ItemUtils.useIgnitable(event.player)) return;
+    this.ignite(event.block, options);
+  }
+}
