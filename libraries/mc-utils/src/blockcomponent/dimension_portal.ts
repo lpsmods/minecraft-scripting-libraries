1eaba70be8027033625a2de30e3497b78a1ee19b
Legopitstop <1589lego@gmail.com>
2026-06-03 23:41:24 -0500

    feat(mc-utils): add dimension portal block component

---
+++ libraries/mc-utils/src/blockcomponent/dimension_portal.ts
@@ -0,0 +1,34 @@
+import { BlockComponentTickEvent, BlockCustomComponent, CustomComponentParameters, world } from "@minecraft/server";
+import { object, Struct } from "superstruct";
+
+import { AddonUtils } from "../utils/addon";
+import { BlockBaseComponent, NearbyEntityBlockEvent } from "./base";
+
+export interface BlockPortalOptions {
+  dimensionId: string;
+}
+
+export class BlockPortalComponent extends BlockBaseComponent implements BlockCustomComponent {
+  static readonly componentId = AddonUtils.makeId("portal");
+  struct: Struct<any, any> = object({});
+
+  /**
+   * Portal block behavior.
+   */
+  constructor() {
+    super();
+    this.onTick = this.onTick.bind(this);
+  }
+
+  // Events
+
+  onNearbyEntityTick(event: NearbyEntityBlockEvent, args: CustomComponentParameters): void {
+    const options = this.struct.create(args.params) as BlockPortalOptions;
+    const dimension = world.getDimension(options.dimensionId ?? "overworld");
+    event.entity.teleport({ x: 0, y: 0, z: 0 }, { dimension });
+  }
+
+  onTick(event: BlockComponentTickEvent, args: CustomComponentParameters) {
+    super.enterLeaveTick(event, args);
+  }
+}
